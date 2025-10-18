0c450f1a8953c7364c6af0c1c358627ab0a9787b
Nikhil Victus <menikhilraj05@gmail.com>
2025-10-18 04:54:01 -0500

    Implemented Controller

---
+++ src/main/java/com/nikhil/employeemanager/service/EmployeeService.java
@@ -0,0 +1,48 @@
+package com.nikhil.employeemanager.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nikhil.employeemanager.model.Employee;
+import com.nikhil.employeemanager.repo.EmployeeRepo;
+import com.nikhil.employeemanager.exception.*;;
+
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepo employeeRepo ; 
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo){
+        this.employeeRepo = employeeRepo;
+    }
+
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+    
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+
+    }
+
+        public Employee findEmployeeById(Long id){
+       return employeeRepo.findEmployeeById(id)
+            .orElseThrow(() -> new UserNotFoundException("User by id " + id + " is not found"));
+        }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
